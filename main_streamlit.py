6887ff5b679e5812f40b68dbe3dc31f724e3ea6f
Ahmad Rizki <amrizjr@gmail.com>
2024-07-12 16:22:48 +0700

    add nestasync

--- main_streamlit.py
+++ main_streamlit.py
@@ -1,3 +1,4 @@
+import nest_asyncio
 import streamlit as st
 from utils.config import get_config_streamlit
 from utils.vector_store import PineconeIndexManager
@@ -8,6 +9,7 @@ from utils.chat_history import ChatHistory
 import hmac
 
 # Apply asyncio and load environment variables
+nest_asyncio.apply()
 load_dotenv()
 
 config = get_config_streamlit()
