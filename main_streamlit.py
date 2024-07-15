79c76b89b966f6950da3084875884913a5caf93d
Ahmad Rizki <amrizjr@gmail.com>
2024-07-15 15:33:52 +0700

    in-one-prompt chain strategy

--- main_streamlit.py
+++ main_streamlit.py
@@ -4,11 +4,11 @@ from utils.vector_store import PineconeIndexManager
 from utils.model_config import ModelName, get_model
 from utils.rag_chain_with_chat_history import create_chain_with_chat_history
 from dotenv import load_dotenv
-from utils.chat_history import ChatHistory
 import hmac
 from utils.config import get_config_streamlit
 from langchain_cohere import CohereRerank
 from langchain.retrievers import ContextualCompressionRetriever
+from utils.chat_history import ChatStore
 
 
 # Apply asyncio and load environment variables
@@ -39,24 +39,23 @@ def check_password():
 
 
 @st.cache_resource(show_spinner=False)
-def load_chain(config: dict = None, top_k: int = 10, top_n: int = 6, model_name: ModelName = ModelName.AZURE_OPENAI, template: str = None, answer_template: str = None):
+def load_chain(config: dict = None, top_k: int = 10, top_n: int = 6, model_name: ModelName = ModelName.AZURE_OPENAI, prompt: str = None, _chat_store: ChatStore = ChatStore()):
 
     llm_model, embed_model = get_model(model_name=model_name, config=config)
 
     pinecone = PineconeIndexManager(
         index_name='ojk', embed_model=embed_model, config=config)
     vector_store = pinecone.load_vector_index()
-    # retriever = vector_store.as_retriever(
-    #     search_type="similarity", search_kwargs={"k": top_k})
     compressor = CohereRerank(
         cohere_api_key=config['cohere_api_key'], top_n=top_n)
     retriever = ContextualCompressionRetriever(
         base_compressor=compressor, base_retriever=vector_store.as_retriever(
             search_type="similarity", search_kwargs={"k": top_k}),
     )
+
     chain = create_chain_with_chat_history(
-        contextualize_q_prompt_str=template,
-        qa_system_prompt_str=answer_template,
+        prompt_str=prompt,
+        get_session_history= _chat_store.get_session_history,
         retriever=retriever,
         llm_model=llm_model,
     )
@@ -64,48 +63,39 @@ def load_chain(config: dict = None, top_k: int = 10, top_n: int = 6, model_name:
 
 
 # ============================== PROMPTING ==============================
-_TEMPLATE = """Given the following conversation and a follow-up question, \
-rephrase the follow-up question to be a standalone question in its original language. 
-If the follow-up question is not clear, indicate so. 
-If the chat history is not relevant to the follow-up question, please ignore the chat history.
-
-Chat History:
-{chat_history}
-
-Follow-up Question: {question}
-Standalone Question: """
-
-_ANSWER_TEMPLATE = """The context information is below.
+PROMPT = """The context information is below.
 Context: 
 {context}
 
 Based on the context and the metadata information provided, \
 answer the query related to banking compliance in Indonesia.
-Use the context and metadata information only, without relying on prior knowledge. 
+Use the context and metadata information only, WITHOUT RELYING ON EXTERNAL KNOWLEDGE.
 ALWAYS ANSWER IN THE USER'S LANGUAGE.
 
-Please provide your answer in the following format, \
-including the regulation number and file URL if available:
+Please provide your answer in the following markdown format, \
+including the regulation number and file URL from metadata if available:
 
-(Answer...) \n\n
+'''
+[Your answer here] \n\n
 Source: [metadata['regulation_number']](metadata['file_url'])
+'''
 
-If you cannot find the regulation number, just provide the (Answer...). 
+If you cannot find the regulation number, just provide the answer without the source. 
 If the file_url ends with '.pdf', you can add the metadata['page_number'] in the URL like this: 
 
-(Answer...) \n\n
-Source: [metadata['regulation_number']](metadata['file_url#page=metadata['page_number']')
+'''
+[Your answer here] \n\n
+Source: [metadata['regulation_number']](metadata['file_url']#page=metadata['page_number'])
+'''
 
-(Answer...) is the answer to the question, don't write '(Answer...)' in the answer.
 DO NOT PROVIDE AMBIGUOUS ANSWERS.
-
-Question: {question}
+IF THE QUESTION IS NOT RELEVANT TO THE CONTEXT, PLEASE SKIP THE QUESTION.
 """
-
 
 # ============================== CONSTANTS ==============================
 TOP_K = 10
 TOP_N = 6
+K_STORE = 3
 model_name = ModelName.AZURE_OPENAI
 
 # ============================== MAIN ==============================
@@ -123,17 +113,25 @@ if "messages" not in st.session_state:
     ]
 
 
-# Initialize chain and chat history
+# Initialize state variables
+if "user_id" not in st.session_state:
+    st.session_state.user_id = "xmriz"
+
+if "conversation_id" not in st.session_state:
+    st.session_state.conversation_id = "ojk-conversation"
+
+if "chat_store" not in st.session_state:
+    st.session_state.chat_store = ChatStore(k=K_STORE)
+
 if "chain" not in st.session_state:
-    st.session_state.chain = load_chain(config=config, top_k=TOP_K, top_n=TOP_N, model_name=model_name,
-                                        template=_TEMPLATE, answer_template=_ANSWER_TEMPLATE)
+    st.session_state.chain = load_chain(config=config, top_k=TOP_K, top_n=TOP_N, model_name=model_name, prompt=PROMPT, _chat_store=st.session_state.chat_store)
 
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = ChatHistory()
 
+# Password check
 if not check_password():
     st.stop()
 
+# Page title
 st.title("Chat with the OJK BOT 💬🤖")
 
 # User input handling
@@ -145,7 +143,6 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         placeholder = st.empty()
@@ -153,24 +150,25 @@ if st.session_state.messages[-1]["role"] != "assistant":
         with st.spinner("Generating response..."):
             answer_chunks = []
             for chunk in st.session_state.chain.stream(
-                {"chat_history": st.session_state.chat_history, "question": prompt}
+                {"question": prompt},
+                config={
+                    "configurable": {
+                        "user_id": st.session_state.user_id,
+                        "conversation_id": st.session_state.conversation_id,
+                    }
+                }
             ):
-                if "question" in chunk:
-                    question = chunk["question"]
-                    print(question)
                 if "answer" in chunk:
                     answer_chunks.append(chunk["answer"])
-                    # Update the placeholder with the current answer
                     placeholder.write("".join(answer_chunks))
 
             answer = "".join(answer_chunks)
-            st.session_state.chat_history.add_chat(prompt, answer)
             message = {"role": "assistant", "content": answer}
             st.session_state.messages.append(message)
 
 # Reset button
 if st.button("Reset"):
-    st.session_state.chat_history.clear_history()
+    st.session_state.chat_store.clear_session_history(user_id=st.session_state.user_id, conversation_id=st.session_state.conversation_id)
     st.session_state.messages = [
         {
             "role": "assistant",
--- utils/rag_chain_with_chat_history.py
+++ utils/rag_chain_with_chat_history.py
@@ -1,17 +1,16 @@
 import json
-from operator import itemgetter
-
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
-from langchain_core.runnables import RunnableMap, RunnablePassthrough
-
-from utils.chat_history import ChatHistory
-
 from langchain_core.retrievers import BaseRetriever
-
-from utils.final_chain_input_type import InputTypeFinalChain
-
 from utils.model_config import ModelName
+from operator import itemgetter
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import (
+    ConfigurableFieldSpec,
+    RunnablePassthrough,
+)
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.output_parsers.string import StrOutputParser
+import json
 
 # ===== formatting functions =====
 
@@ -26,64 +25,148 @@ def _format_metadata(metadata):
 
 def _combine_documents(docs):
     """Combine documents into a single JSON string."""
-    doc_list = [{"metadata": _format_metadata(doc.metadata), "page_content": doc.page_content} for doc in docs]
+    doc_list = [{"metadata": _format_metadata(
+        doc.metadata), "page_content": doc.page_content} for doc in docs]
     return json.dumps(doc_list, indent=2)
 
 
-def create_chain_with_chat_history(contextualize_q_prompt_str: str, qa_system_prompt_str: str, retriever: BaseRetriever, llm_model: ModelName):
-    CONTEXTUALIZE_Q_PROMPT_STR = contextualize_q_prompt_str
-    QA_SYSTEM_PROMPT_STR = qa_system_prompt_str
-    QA_PROMPT = ChatPromptTemplate.from_template(QA_SYSTEM_PROMPT_STR)
-    CONTEXTUALIZE_Q_PROMPT = PromptTemplate.from_template(CONTEXTUALIZE_Q_PROMPT_STR)
-    _inputs_question = RunnableMap(
-        standalone_question=RunnablePassthrough.assign(
-            chat_history=lambda x: x["chat_history"].get_formatted_history()
-        )
-        | CONTEXTUALIZE_Q_PROMPT
-        | llm_model
-        | StrOutputParser(),
+# ===== Chaining =====
+
+def create_chain_with_chat_history(prompt_str: str, retriever: BaseRetriever, llm_model: ModelName, get_session_history: BaseChatMessageHistory):
+    PROMPT = ChatPromptTemplate.from_messages(
+        [
+            ("system", prompt_str),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{question}"),
+        ]
     )
-    _context_chain = {
-        "context": itemgetter("standalone_question") | retriever | _combine_documents,
-        "question": lambda x: x["standalone_question"],
+
+    context = itemgetter("question") | retriever | _combine_documents
+    context_chain = RunnablePassthrough.assign(context=context)
+
+    full_chain = context_chain | {
+        "question": itemgetter("question"),
+        "answer": PROMPT | llm_model | StrOutputParser(),
+        "context": itemgetter("context"),
     }
-    conversational_qa_with_context_chain = (
-        _inputs_question
-        | _context_chain
-        | {
-            "question": itemgetter("question"),
-            "answer": QA_PROMPT | llm_model | StrOutputParser(),
-            "context": itemgetter("context"),
-        }
-        # | StrOutputParser()
+
+    chain_with_message_history = RunnableWithMessageHistory(
+        full_chain,
+        get_session_history=get_session_history,
+        input_messages_key="question",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+        history_factory_config=[
+            ConfigurableFieldSpec(
+                id="user_id",
+                annotation=str,
+                name="User ID",
+                description="Unique identifier for the user.",
+                default="",
+                is_shared=True,
+            ),
+            ConfigurableFieldSpec(
+                id="conversation_id",
+                annotation=str,
+                name="Conversation ID",
+                description="Unique identifier for the conversation.",
+                default="",
+                is_shared=True,
+            ),
+        ],
     )
-    final_chain = conversational_qa_with_context_chain.with_types(input_type=InputTypeFinalChain)
-    return final_chain
+
+    return chain_with_message_history
 
 
-def get_response(chat_history: ChatHistory, question: str, chain):
-    response = chain.invoke({"chat_history": chat_history, "question": question})
-    chat_history.add_chat(response["question"], response["answer"])
+def get_response(question: str, chain, user_id: str, conversation_id: str):
+    response = chain.invoke(
+        {"question": question},
+        config={
+            "configurable": {"user_id": user_id, "conversation_id": conversation_id}
+        },
+    )
     return response
 
 
-
-async def print_answer_stream(chat_history: ChatHistory, question: str, chain):
-    answer_chunks = []
-    async for chunk in chain.astream({"chat_history": chat_history, "question": question}):
-        if 'question' in chunk:
-            question = chunk['question']
+def print_answer_stream(question: str, chain, user_id: str, conversation_id: str):
+    for chunk in chain.stream({"question": question}, config={"configurable": {"user_id": user_id, "conversation_id": conversation_id}}):
         if 'answer' in chunk:
-            answer_chunks.append(chunk['answer'])
             print(chunk['answer'], end='', flush=True)
 
-    answer = ''.join(answer_chunks)
-    chat_history.add_chat(question, answer)
+
+# # ===== Chaining =====
+# PROMPT = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", _ANSWER_TEMPLATE),
+#         MessagesPlaceholder(variable_name="chat_history"),
+#         ("human", "{question}"),
+#     ]
+# )
+
+# context = itemgetter("question") | retriever | _combine_documents
+# context_chain = RunnablePassthrough.assign(context=context)
+
+# full_chain = context_chain | {
+#     "question": itemgetter("question"),
+#     "answer": PROMPT | llm_model | StrOutputParser(),
+#     "context": itemgetter("context"),
+# }
+
+
+# def create_chain_with_chat_history(contextualize_q_prompt_str: str, qa_system_prompt_str: str, retriever: BaseRetriever, llm_model: ModelName):
+#     CONTEXTUALIZE_Q_PROMPT_STR = contextualize_q_prompt_str
+#     QA_SYSTEM_PROMPT_STR = qa_system_prompt_str
+#     QA_PROMPT = ChatPromptTemplate.from_template(QA_SYSTEM_PROMPT_STR)
+#     CONTEXTUALIZE_Q_PROMPT = PromptTemplate.from_template(CONTEXTUALIZE_Q_PROMPT_STR)
+#     _inputs_question = RunnableMap(
+#         standalone_question=RunnablePassthrough.assign(
+#             chat_history=lambda x: x["chat_history"].get_formatted_history()
+#         )
+#         | CONTEXTUALIZE_Q_PROMPT
+#         | llm_model
+#         | StrOutputParser(),
+#     )
+#     _context_chain = {
+#         "context": itemgetter("standalone_question") | retriever | _combine_documents,
+#         "question": lambda x: x["standalone_question"],
+#     }
+#     conversational_qa_with_context_chain = (
+#         _inputs_question
+#         | _context_chain
+#         | {
+#             "question": itemgetter("question"),
+#             "answer": QA_PROMPT | llm_model | StrOutputParser(),
+#             "context": itemgetter("context"),
+#         }
+#         # | StrOutputParser()
+#     )
+#     final_chain = conversational_qa_with_context_chain.with_types(input_type=InputTypeFinalChain)
+#     return final_chain
+
+
+# def get_response(chat_history: ChatHistory, question: str, chain):
+#     response = chain.invoke({"chat_history": chat_history, "question": question})
+#     chat_history.add_chat(response["question"], response["answer"])
+#     return response
+
+
+# async def print_answer_stream(chat_history: ChatHistory, question: str, chain):
+#     answer_chunks = []
+#     async for chunk in chain.astream({"chat_history": chat_history, "question": question}):
+#         if 'question' in chunk:
+#             question = chunk['question']
+#         if 'answer' in chunk:
+#             answer_chunks.append(chunk['answer'])
+#             print(chunk['answer'], end='', flush=True)
+
+#     answer = ''.join(answer_chunks)
+#     chat_history.add_chat(question, answer)
 
 
 # # ===== Prompt Templates =====
 
-# CONTEXTUALIZE_Q_PROMPT_STR = """Given the following conversation and a follow up question, rephrase the 
+# CONTEXTUALIZE_Q_PROMPT_STR = """Given the following conversation and a follow up question, rephrase the
 # follow up question to be a standalone question WITH ITS ORIGINAL LANGUAGE. if the follow \
 # up question is not clear.
 
@@ -99,10 +182,10 @@ async def print_answer_stream(chat_history: ChatHistory, question: str, chain):
 # context: {context}
 
 # Given the context and the metadata information and not prior knowledge, \
-# answer the query asking about banking compliance in Indonesia. 
+# answer the query asking about banking compliance in Indonesia.
 # Answer the question based on the context and the metadata information.
 # ALWAYS ANSWER WITH USER'S LANGUAGE.
-# Please provide your answer with [regulation_number](file_url) in metadata 
+# Please provide your answer with [regulation_number](file_url) in metadata
 # (if possible) in the following format:
 
 # Answer... \n\n
@@ -142,4 +225,3 @@ async def print_answer_stream(chat_history: ChatHistory, question: str, chain):
 # )
 
 # final_chain = conversational_qa_with_context_chain.with_types(input_type=InputTypeFinalChain)
-
